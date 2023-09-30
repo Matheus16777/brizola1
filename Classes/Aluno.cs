c8d9f7ecb53e628129166ffbc09bb39396686cb6
Matheus Antonio Brizola Ramos <brizolamatheus61@gmail.com>
2023-09-29 21:01:22 -0300

    iniciodorepositorio

---
+++ Classes/Aluno.cs
@@ -0,0 +1,67 @@
+﻿using Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+   
+    }
+internal class Aluno
+{
+    public string nome;
+    public int idade;
+    public int alunos;
+    public Aluno(string nome, int idade)
+    {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public int Idade
+    {
+        get
+        {
+            return idade;
+
+        }
+        set
+        {
+            if (value > 0 && value <= 20)
+            {
+                idade = value;
+            }
+            else
+            {
+                throw new Exception("Valor invalido para idade");
+            }
+
+
+        }
+    }
+
+    public Aluno(string nome, int idade, int aluno)
+    {
+        this.nome = nome;
+        this.idade = idade;
+        alunos = aluno;
+    }
+    private double  nota1;
+    private double nota2;
+
+    private double media()
+    {
+        return (nota1 + nota2) / 2;
+    }
+    public void passouDeAno();
+    {
+      Console.WriteLine("Digite a 1 nota");
+    nota1 = Convert.ToDouble(Console.ReadLine)());
+    }
+
+}
+
+
---
+++ Classes/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Aluno aluno1 = new Aluno("Matheus", 18);
+           
+            Aluno aluno2 = new Aluno("Lucas",20);
+           
+            Console.WriteLine("Aluno 1");
+            Console.WriteLine(aluno1.nome);
+            Console.WriteLine(aluno1.idade);
+
+           
+
+
+
+            Console.WriteLine("Aluno 2");
+            Console.WriteLine(aluno2.nome);
+            Console.WriteLine(aluno2.idade);
+
+            aluno2.passouDeAno();
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
